ca474143a1e57fbfe5a47ecab3cf54325e491637
xuantung412 <xuan.tung.nguyen811@gmail.com>
2021-12-03 21:50:13 +1030

    small change.

--- src/SimpleGame/SimpleGameEngineTesting.java
+++ src/SimpleGame/SimpleGameEngineTesting.java
@@ -1,38 +1,38 @@
 package SimpleGame;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class SimpleGameEngineTesting {
+public class SimpleGameEngineTesting {
 	MainGame test01 = new MainGame();
 
 	@Test
-	void testCase01() {
+	public void testCase01() {
 		test01.systemRandomNum =1; //Set system chose Paper
 		assertEquals("Congraduation, You Win",test01.play(2));//User chose Sic
 				
 	}
 	@Test
-	void testCase02() {
+	public void testCase02() {
 		test01.systemRandomNum =1; //Set system chose Paper
 		assertEquals("No Luck, You Lose",test01.play(0));//User chose Rock
 
 	}
 	@Test
-	void testCase03() {
+	public void testCase03() {
 		test01.systemRandomNum =0; //Set system chose Rock
 		assertEquals("Congraduation, You Win",test01.play(1));//User chose Paper
 
 	}
 	@Test
-	void testCase04() {
+	public void testCase04() {
 		test01.systemRandomNum =0; //Set system chose Rock
 		assertEquals("No Luck, You Lose",test01.play(2));//User chose Sic
 
 	}
 	@Test
-	void testCase05() {
+	public void testCase05() {
 		test01.systemRandomNum =2; //Set system chose Sic
 		assertEquals("No Luck, You Lose",test01.play(1));//User chose Paper
 
