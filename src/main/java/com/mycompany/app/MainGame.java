c62847fca9f2b44283fdb791ed94f4d1df38729c
xuantung412 <xuan.tung.nguyen811@gmail.com>
2021-12-04 13:25:03 +1030

    fix dir to run UnitTest on Jenkins

---
+++ src/main/java/com/mycompany/app/MainGame.java
@@ -0,0 +1,56 @@
+package com.mycompany.app;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class MainGame {
+	public int systemRandomNum;
+	public void getRandomNum(){
+		Random ranNum = new Random(0);
+		int randomNumber = ranNum.nextInt(2);
+		this.systemRandomNum = randomNumber;
+	}
+	public String play(int userInput) {
+
+		//Random number 0 is Rock, 1 is paper and 2 is scissors
+		if(userInput == systemRandomNum) {
+			return "Draw";
+		}
+		else if(systemRandomNum == 0 && userInput == 1){
+			return "No Luck, You Lose";	//Wrong code here
+
+		}
+		else if(systemRandomNum == 0 && userInput == 2){
+			return "Congraduation, You Win";	//Wrong code here
+		}
+		else if(systemRandomNum == 1 && userInput == 0){
+			return "No Luck, You Lose";
+		}
+		else if(systemRandomNum == 1 && userInput == 2){
+			return "Congraduation, You Win";
+		}
+		else if(systemRandomNum == 2 && userInput == 1){
+			return "No Luck, You Lose";
+		}
+		else if(systemRandomNum == 2 && userInput == 0){
+			return "Congraduation, You Win";
+		}
+		else {
+			return "Error!";
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Rock Paper Scissors Game\n1. Rock \t 2.Paper\t 3.Sissor\nPlease enter number(1-3)");
+		Scanner scan = new Scanner(System.in);
+		int userInput = scan.nextInt();
+		System.out.println("System Drawing...");
+		userInput -= 1;
+		MainGame newGame = new MainGame();
+		newGame.getRandomNum();
+		System.out.println(newGame.play(userInput));
+
+	}
+}
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,43 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+
+public class AppTest {
+	MainGame test01 = new MainGame();
+
+	@Test
+	public void testCase01() {
+		test01.systemRandomNum =1; //Set system chose Paper
+		assertEquals("Congraduation, You Win",test01.play(2));//User chose Sic
+
+	}
+	@Test
+	public void testCase02() {
+		test01.systemRandomNum =1; //Set system chose Paper
+		assertEquals("No Luck, You Lose",test01.play(0));//User chose Rock
+
+	}
+	@Test
+	public void testCase03() {
+		test01.systemRandomNum =0; //Set system chose Rock
+		assertEquals("Congraduation, You Win",test01.play(1));//User chose Paper
+
+	}
+	@Test
+	public void testCase04() {
+		test01.systemRandomNum =0; //Set system chose Rock
+		assertEquals("No Luck, You Lose",test01.play(2));//User chose Sic
+
+	}
+	@Test
+	public void testCase05() {
+		test01.systemRandomNum =2; //Set system chose Sic
+		assertEquals("No Luck, You Lose",test01.play(1));//User chose Paper
+
+	}
+}
