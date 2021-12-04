cffa13dbd2cbf48216889c401836162ab1f87589
xuantung412 <xuan.tung.nguyen811@gmail.com>
2021-12-04 12:03:38 +1030

    Add Test testng.xml to run test

--- src/test/java/SimpleGameEngineTesting.java
+++ src/test/java/SimpleGameEngineTesting.java
@@ -24,13 +24,13 @@ public class SimpleGameEngineTesting {
 	@Test
 	public void testCase03() {
 		test01.systemRandomNum =0; //Set system chose Rock
-		assertEquals("Congraduation, You Win",test01.play(2));//User chose Paper
+		assertEquals("Congraduation, You Win",test01.play(1));//User chose Paper
 
 	}
 	@Test
 	public void testCase04() {
 		test01.systemRandomNum =0; //Set system chose Rock
-		assertEquals("No Luck, You Lose",test01.play(1));//User chose Sic
+		assertEquals("No Luck, You Lose",test01.play(2));//User chose Sic
 
 	}
 	@Test
