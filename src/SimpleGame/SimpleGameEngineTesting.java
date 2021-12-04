81984e68cea56f91703d4ed571d0b34b32b48773
xuantung412 <xuan.tung.nguyen811@gmail.com>
2021-12-04 10:34:19 +1030

    Convert back

--- src/SimpleGame/SimpleGameEngineTesting.java
+++ src/SimpleGame/SimpleGameEngineTesting.java
@@ -1,51 +1,39 @@
-package com.mycompany.app;
+package SimpleGame;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue1()
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue2()
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue3()
-    {
-        assertTrue( true );
-    }
-    
-    
-    @Test
-    public void shouldAnswerWithTrue4()
-    {
-        assertTrue( true );
-    }
-    
-    @Test
-    public void shouldAnswerWithTrue5()
-    {
-        assertTrue( true );
-    }
-}
+public class SimpleGameEngineTesting {
+	MainGame test01 = new MainGame();
+
+	@Test
+	public void testCase01() {
+		test01.systemRandomNum =1; //Set system chose Paper
+		assertEquals("Congraduation, You Win",test01.play(2));//User chose Sic
+
+	}
+	@Test
+	public void testCase02() {
+		test01.systemRandomNum =1; //Set system chose Paper
+		assertEquals("No Luck, You Lose",test01.play(0));//User chose Rock
+
+	}
+	@Test
+	public void testCase03() {
+		test01.systemRandomNum =0; //Set system chose Rock
+		assertEquals("Congraduation, You Win",test01.play(1));//User chose Paper
+
+	}
+	@Test
+	public void testCase04() {
+		test01.systemRandomNum =0; //Set system chose Rock
+		assertEquals("No Luck, You Lose",test01.play(2));//User chose Sic
+
+	}
+	@Test
+	public void testCase05() {
+		test01.systemRandomNum =2; //Set system chose Sic
+		assertEquals("No Luck, You Lose",test01.play(1));//User chose Paper
+
+	}
